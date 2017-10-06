0a23b392b8c04bc6da47eacacf9e6ee97078b456
Smitha <spallaki@brynmawr.edu>
2017-10-06 15:23:32 -0700

    Add list buttons

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>JK she da best</p>,
+var dummyData = ['homework', 'cook', 'walk', 'shower', 'sleep'];
+
+class TodoList extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: ''
+    }
+  }
+
+  render() {
+    return(
+
+        // <input type="submit" value="X" />
+
+      <ul>
+       {dummyData.map((dummyData) => (<li> <button>
+          X
+       </button>{dummyData}</li>))}
+    </ul>
+  )
+  }
+}
+
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
