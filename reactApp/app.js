fc350bbc89fb355acabe44b002964086ecadeafb
Smitha <spallaki@brynmawr.edu>
2017-10-06 16:17:04 -0700

    render InputLine and TodoList

--- reactApp/app.js
+++ reactApp/app.js
@@ -8,23 +8,45 @@ class TodoList extends React.Component{
     super(props);
     this.state = {
       task: ''
-    }
   }
-
+}
   render() {
     return(
-
-        // <input type="submit" value="X" />
-
       <ul>
-       {dummyData.map((dummyData) => (<li> <button>
-          X
-       </button>{dummyData}</li>))}
-    </ul>
-  )
+       {dummyData.map((dummyData) => (
+         <li><button> X </button>{dummyData}</li>)
+       )}
+     </ul>
+  )}
+}
+
+class InputLine extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <form>
+          <input type='text' placeholder='task' />
+          <input type='submit' value='Add todo'/>
+     </form>
+    )
+  }
+}
+
+class TodoApp extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <InputLine />
+        <TodoList />
+      </div>
+    )
   }
 }
 
 
-ReactDOM.render(<TodoList />,
-   document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
